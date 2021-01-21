bf9e83abe1eefb0b4a60587423f735c60bd5086f
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2021-01-21 12:14:30 -0700

    e

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 // Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
- return res.send("Hello World");
+ return res.send("Hello World via github actions");
 });
 // listen to port 7000 by default
 app.listen(process.env.PORT || 7000, () => {
