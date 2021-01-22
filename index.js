bb4b215d4be91c90a1bee50f88bf299c3368e861
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2021-01-21 17:52:29 -0700

    sw

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get("/", function (req, res) {
 app.listen(process.env.PORT || 7000, () => {
   console.log("Server is running on 7000");
 });
-console.log("appre lo");
+console.log("appvre lo");
 module.exports = app;
