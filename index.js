0df5562277258b4637a67b35fa65cba801736cca
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2021-01-22 09:21:07 -0700

    it works!

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 // Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
- return res.send("Hello World via github actions");
+ return res.send("Hello World");
 });
 // listen to port 7000 by default
 app.listen(process.env.PORT || 7000, () => {
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const app = require("../index");
 describe("GET /", () => {
-  it("respond with Hello World via github actions", (done) => {
-    request(app).get("/").expect("Hello World via github actions", done);
+  it("respond with Hello World", (done) => {
+    request(app).get("/").expect("Hello World", done);
   })
 });
