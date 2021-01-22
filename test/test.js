662bd50ac211f3588f35e22f76fdd69e4537d9d1
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2021-01-22 09:54:22 -0700

    yay!

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const app = require("../index");
 describe("GET /", () => {
-  it("respond with Hello World", (done) => {
-    request(app).get("/").expect("Hello World", done);
+  it("respond with It works Clark!", (done) => {
+    request(app).get("/").expect("It works Clark!", done);
   })
 });
