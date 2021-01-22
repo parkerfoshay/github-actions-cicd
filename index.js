177d247f6d9333e34b15efe888592153e850e76f
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2021-01-22 11:26:20 -0700

    linted

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // importing express framework
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 // Respond with "hello world" for requests that hit our root "/"
-app.get("/", function (req, res) {
- return res.send("It works Clark!");
-});
+app.get('/', function (req, res) {
+  return res.send('It works Clark!')
+})
 // listen to port 7000 by default
 app.listen(process.env.PORT || 7000, () => {
-  console.log("Server is running on 7000");
-});
-console.log("appovvre ");
-module.exports = app;
+  console.log('Server is running on 7000')
+})
+console.log('appovvre ')
+module.exports = app
