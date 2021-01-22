308d2fa831a1aa96d8590b553c2f5ce1d2841f66
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2021-01-22 12:21:39 -0700

    it works clark!

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
-const request = require("supertest");
-const app = require("../index");
-describe("GET /", () => {
-  it("respond with It works Clark!", (done) => {
-    request(app).get("/").expect("It works Clark!", done);
+const request = require('supertest')
+const app = require('../index')
+const { describe, it } = require('mocha')
+describe('GET /', () => {
+  it('respond with It works Clark!', (done) => {
+    request(app).get('/').expect('It works Clark!', done)
   })
-});
+})
